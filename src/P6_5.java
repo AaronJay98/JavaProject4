2b5a3594a3d8284c181b23c83eb0d490dac9b3d9
Aaron Jay Macaldo <aaronjay.macaldo@sjsu.edu>
2019-09-19 20:48:54 -0700

    Added comments on class and P6_5 and also deleted unnecessary code in isPrime method

--- src/P6_5.java
+++ src/P6_5.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 public class P6_5 {
 
     public static void main(String[] args) {
-        Scanner sUserInput = new Scanner(System.in);
-        System.out.println("Welcome to my program that prints out all prime numbers to your indicated integer.");
-        int iUserInt;
-        System.out.println("Please enter an integer: ");
-        iUserInt = sUserInput.nextInt();
-        PrimeGenerator pgUsersPrimes = new PrimeGenerator(iUserInt);
-        int iPrimeInt = 0;
+        Scanner sUserInput = new Scanner(System.in);            //gets the users input
+        System.out.println("Welcome to my program that prints out all prime numbers to your indicated integer.");   //welcome message describing program
+        int iUserInt;                                           //Will hold the users input int
+        System.out.println("Please enter an integer: ");        //Ask user to input int
+        iUserInt = sUserInput.nextInt();                        //Puts users int into the appropriate variable
+        PrimeGenerator pgUsersPrimes = new PrimeGenerator(iUserInt);    //Creates a PrimeGenerator object and passes the users int as a parameter
+        int iPrimeInt = 0;                                              //Will hold the prime numbers to be printed or hold -1 to stop printing and end loop
 
 
         System.out.println("Here are all the prime numbers up to " + iUserInt + ":\n");
-        if(iUserInt <= 1)
-            System.out.println("None");
+        if(iUserInt <= 1)                                               //Outputs that there are no primes when a user inputs a number before the first prime number (2)
+            System.out.println("None");                                 //
         else {
-            while(true) {
-                iPrimeInt = pgUsersPrimes.nextPrime();
-                if(iPrimeInt != -1) {
-                    System.out.println(iPrimeInt);
+            while(true) {                                               //This while loop will loop until it breaks
+                iPrimeInt = pgUsersPrimes.nextPrime();                  //Gets the next prime (starting from first) up to the user input number. (.nextPrime() returns -1 when surpassed user input number)
+                if(iPrimeInt != -1) {                                   //Check to see if iPrimeInt is a prime number or -1
+                    System.out.println(iPrimeInt);                      //If true: Prints the Prime Number
                 }
-                else
+                else                                                    //If false: no more prime numbers to be printed and breaks loop and ends program
                     break;
             }
         }
--- src/edu/sjsu/macaldo/aaronjay/PrimeGenerator.java
+++ src/edu/sjsu/macaldo/aaronjay/PrimeGenerator.java
@@ -2,35 +2,32 @@ package edu.sjsu.macaldo.aaronjay;
 
 
 public class PrimeGenerator {
-    public PrimeGenerator(int inputInt) {
+    public PrimeGenerator(int inputInt) {           //constructor that takes userInput and stores it into a private variable
         endInt = inputInt;
     }
 
     public int nextPrime() {
         do {
-            currentInt++;
-        } while(!isPrime(currentInt) && currentInt <= endInt);
-        if(currentInt <= endInt)
-            return currentInt;
+            currentInt++;                                       //Each time nextPrime is called it goes to next int first then checks to see if prime
+        } while(!isPrime(currentInt) && currentInt <= endInt);  //Loops until a prime is found or reached the endInt (userInput)
+        if(currentInt <= endInt)                                //Checks to see if not at end
+            return currentInt;                                  //If not at end, then currentInt is a prime and return it to main
         else
-            return -1;
+            return -1;                                          //If at end, return -1 to signal main that we are at end
     }
 
     private boolean isPrime(int checkInt) {
-        if(checkInt == 2)
-            return true;
-
-        for(int i = 2; i < checkInt; i++) {
-            if(checkInt % i == 0) {
-                return false;
+        for(int i = 2; i < checkInt; i++) {             //Loops up to all numbers before target number
+            if(checkInt % i == 0) {                     //For each number evaluates if it divides checkInt evenly
+                return false;                           //If true: it is not prime and returns false
             }
         }
-        return true;
+        return true;                                    //If it goes through all numbers and none divides it evenly then it is prime and returns true
     }
 
 
 
 
-    private int currentInt = 1;
-    private int endInt = 0;
+    private int currentInt = 1;                         //variable that navigates through each number up to endInt to find a prime
+    private int endInt = 0;                             //number passed to this class when constructed indicating up to what number do we display prime numbers
 }
